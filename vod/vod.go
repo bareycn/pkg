1e7c2fd490df3b28b641b1635fab822941c83c73
Ethan <ethan@barey.cn>
2024-06-12 20:02:20 +0800

    deploy

--- vod/vod.go
+++ vod/vod.go
@@ -91,15 +91,21 @@ func uploadOss(auth *UploadAuth, address *UploadAddress, reader io.Reader) error
 	return nil
 }
 
-func UploadVideo(filename string, file multipart.File) (fileName string, mediaID string, err error) {
-	title := filepath.Base(filename)
+func UploadVideo(file *multipart.FileHeader) (fileName string, mediaID string, err error) {
+	src, err := file.Open()
+	if err != nil {
+		return "", "", err
+	}
+	defer src.Close()
+
+	title := filepath.Base(file.Filename)
 	if len(title) >= 128 {
 		title = uuid.New().String()
 	}
 
 	requestUploadVideo := &vod.CreateUploadVideoRequest{
 		Title:           tea.String(title),
-		FileName:        tea.String(filename),
+		FileName:        tea.String(file.Filename),
 		TemplateGroupId: config.TemplateGroupId,
 		WorkflowId:      config.WorkflowId,
 	}
@@ -111,7 +117,8 @@ func UploadVideo(filename string, file multipart.File) (fileName string, mediaID
 	if err != nil {
 		log.Panicln(err)
 	}
-	err = uploadOss(auth, address, file)
+	log.Println(auth, address, err)
+	err = uploadOss(auth, address, src)
 	if err != nil {
 		return "", "", err
 	}
